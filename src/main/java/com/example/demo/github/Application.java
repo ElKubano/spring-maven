18af7c8e9cf43b783ec231e3776c94e00f625dab
ElKubano <koala.newb@gmail.com>
2025-04-29 16:12:28 +0200

    Update Application.java

--- src/main/java/com/example/demo/github/Application.java
+++ src/main/java/com/example/demo/github/Application.java
@@ -2,6 +2,17 @@ package com.example.demo.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
 
 @SpringBootApplication
 public class Application {
