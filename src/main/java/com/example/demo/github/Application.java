5ddff7ab64e46af7dc89dd73f3b183e59cf72931
ElKubano <koala.newb@gmail.com>
2025-04-29 17:51:03 +0200

    update rapide

--- src/main/java/com/example/demo/github/Application.java
+++ src/main/java/com/example/demo/github/Application.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-}
-
 @RestController
 class HelloWorldController {
 
@@ -21,4 +12,13 @@ class HelloWorldController {
     public String sayHello() {
         return "Hello, World!";
     }
+}
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 }
