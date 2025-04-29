584f03b3b32c65034a8ca73c175815584420bad9
ElKubano <koala.newb@gmail.com>
2025-04-29 16:57:24 +0200

    push modif java

--- src/main/java/com/example/demo/github/Application.java
+++ src/main/java/com/example/demo/github/Application.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-class HelloWorldController {
-
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello, World!";
-    }
-}
-
 @SpringBootApplication
 public class Application {
 
@@ -21,4 +12,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
 }
